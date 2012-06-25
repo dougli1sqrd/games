10091516044f0d5fdc480121744a740e12b2b45a
shakedown <a1979shakedown@gmail.com>
2012-06-25 10:26:26 -0700

    Update master

--- tictactoe/src/main/java/org/brainstorm/games/tictactoe/AIPlayer.java
+++ tictactoe/src/main/java/org/brainstorm/games/tictactoe/AIPlayer.java
@@ -1,17 +1,6 @@
 /*
  * AIPlayer.java
  *
- * created on: 6/6/12 11:04 PM
- * Copyright(c) 2002-2012 Thetus Corporation.  All Rights Reserved.
- *                        www.thetus.com
- *
- * Use of copyright notice does not imply publication or disclosure.
- * THIS SOFTWARE CONTAINS CONFIDENTIAL AND PROPRIETARY INFORMATION CONSTITUTING VALUABLE TRADE SECRETS
- *  OF THETUS CORPORATION, AND MAY NOT BE:
- *  (a) DISCLOSED TO THIRD PARTIES;
- *  (b) COPIED IN ANY FORM;
- *  (c) USED FOR ANY PURPOSE EXCEPT AS SPECIFICALLY PERMITTED IN WRITING BY THETUS CORPORATION.
- *
  * Created using IntelliJ IDEA.
  * SVN status:
  *  $HeadURL: $
