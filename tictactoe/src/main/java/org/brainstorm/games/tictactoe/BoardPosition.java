5561b4277261964df2bc73aace22939baa8f7640
shakedown <a1979shakedown@gmail.com>
2012-06-25 10:54:36 -0700

    Update master

--- tictactoe/src/main/java/org/brainstorm/games/tictactoe/BoardPosition.java
+++ tictactoe/src/main/java/org/brainstorm/games/tictactoe/BoardPosition.java
@@ -1,17 +1,6 @@
 /*
  * BoardPosition.java
  *
- * created on: 6/6/12 11:55 PM
- * Copyright(c) 2002-2012 Thetus Corporation.  All Rights Reserved.
- *                        www.thetus.com
- *
- * Use of copyright notice does not imply publication or disclosure.
- * THIS SOFTWARE CONTAINS CONFIDENTIAL AND PROPRIETARY INFORMATION CONSTITUTING VALUABLE TRADE SECRETS
- *  OF THETUS CORPORATION, AND MAY NOT BE:
- *  (a) DISCLOSED TO THIRD PARTIES;
- *  (b) COPIED IN ANY FORM;
- *  (c) USED FOR ANY PURPOSE EXCEPT AS SPECIFICALLY PERMITTED IN WRITING BY THETUS CORPORATION.
- *
  * Created using IntelliJ IDEA.
  * SVN status:
  *  $HeadURL: $
@@ -26,7 +15,7 @@ package org.brainstorm.games.tictactoe;
 /**
  * Represents a position on the tic tac toe board in terms of rows (or y) 1, 2, and 3 and columns (or x) 1, 2, and 3.
  *
- * @author Nate Wolfe - nwolfe@thetus.com
+ * @author Nate Wolfe 
  */
 public class BoardPosition {
 
